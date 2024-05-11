ebc1c02b736dc37a48ad68955096f8fa9984210a
Wallywallie <499630837@qq.com>
2024-05-11 15:41:56 +0800

    currently capable of generating good chinese answer but it takes too long!!!

--- rag.py
+++ rag.py
@@ -16,11 +16,12 @@ import time
 from transformers import pipeline
 
 import chromadb
-
-
-
-
-
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval_qa.base import VectorDBQA
+from langchain_core.runnables import RunnableParallel,RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
 
 chunk_size = 256
 chunk_overlap = 0
@@ -33,28 +34,29 @@ os.environ['CUDA_VISIBLE_DEVICES']='0,1,2,3'
 model_dir = "./llama/llama-2-70b-chat-hf"
 embeddingmodel_path = "./model/m3e-base"
 
-prompt_urbanist = """
+
+template = """
     # Role: 城市规划师
     ## Profile
     - Author: author
     - Version: 0.1
     - Language: 中文
     - Description: 你是一个有多年工作经验的城市规划师。你非常熟知上海市的城市规划管理规定。
-    ### Skill
-    1. 你十分掌握土地开发和土地利用规划的方法，平衡城市发展的需求与土地资源的供给，确保土地利用的合理性和效益。
-    2. 你十分擅长分析城市社会经济特征和发展趋势，了解居民需求和就业机会，为城市规划提供数据支持和决策参考。
-    3. 你十分熟悉城市规划相关的法律法规和政策，包括国家、地方的规划法规以及土地使用政策，确保规划的合法性和可行性。
-    4. 你非常熟悉城市发展的基本理论、原则和方法，包括城市发展模式、城市功能区划、可持续发展等。
-    5. 你非常熟悉城市设计原则，包括公共空间设计、建筑外观、景观设计等，以创造宜居、宜人的城市环境。
-    6. 你具备非常好的沟通与协调能力和团队协作能力，能够与政府部门、社区居民、开发商等多方合作，推动城市规划的实施。
+    ##Context:{context}
+
     ## Rules
     1. 不要在任何情况下破坏角色。
     2. 不要说废话或编造事实
     3. 不要介绍自己
-    ## Query:两栋平行的居住建筑的间距应该怎样布置？
+
+    ## Query:{input}
+
     ## Initialization
-    作为角色 <Role>, 严格遵守 <Rules>, 使用默认 <Language> 与用户对话，回答用户的<Query>。    
+    作为角色 <Role>, 严格遵守 <Rules>,根据<Context>,使用默认 <Language> 与用户对话，回答用户的<Query>。    
 """
+
+prompt = PromptTemplate.from_template(template)
+
 
 #本地数据加载
 def load_txt(file_path : str):
@@ -111,14 +113,8 @@ def embedding(documents, embedding, db):
     return db
 
 
-
-
-t0 = time.perf_counter()
 embedding_func = embedding_function() 
 db = load_db(db_path, embedding_func)
-t1 = time.perf_counter()
-print(f"Loading database : took {t1 - t0} seconds to execute")
-
 
 if load_new:
 
@@ -127,22 +123,16 @@ if load_new:
     db = embedding(split_documents, embedding_func, db)    
 
 
-
 generator = pipeline(task="text-generation", model=model_dir,torch_dtype=torch.bfloat16, device_map="auto", max_new_tokens = 1024)
-t2 = time.perf_counter()
-print(f"Loading tokenizer and model : took {t2 - t1} seconds to execute")
-template = """Question: {question}
-Answer:
-"""
-prompt = PromptTemplate.from_template(template)
 
-
-# LLM选型
 pipeline = HuggingFacePipeline(pipeline = generator)
 retriever = db.as_retriever()
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-qa = ConversationalRetrievalChain.from_llm(pipeline, retriever,memory=memory)
 
+
+output_parser = StrOutputParser()
+setup_and_retriever = RunnableParallel({"context" : retriever, "input": RunnablePassthrough()})
+chain = setup_and_retriever | prompt | pipeline | output_parser
 
 
 
--- web_page.py
+++ web_page.py
@@ -1,24 +1,11 @@
 import streamlit as st 
 import rag
 
-
-
 # Customize the layout
 st.set_page_config(page_title="RAG AI", page_icon=":robot:", layout="wide", )     
-st.markdown(f"""
-            <style>
-            .stApp {{background-image: url("https://images.unsplash.com/photo-1509537257950-20f875b03669?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80"); 
-                    background-attachment: fixed;
-                    background-size: cover}}
-        </style>
-        """, unsafe_allow_html=True)
 
-    
 question = st.text_input("Ask something from the file", placeholder="Find something similar to: ....this.... in the text?")    
 
-
-
-
 if question:
-    response = rag.qa({"question": question})
+    response = rag.chain.invoke(question)
     st.write(response) 
